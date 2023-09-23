60e8abb60edd8913b9524467d71940c41c4ba508
Saro9033 <ssaravanan2310@gmail.com>
2023-09-23 17:20:20 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="/Landing-page-wit">
-    <BrowserRouter>
+  <React.StrictMode >
+    <BrowserRouter basename="/Landing-page-with-router">
     <App />
     </BrowserRouter>
   </React.StrictMode>
